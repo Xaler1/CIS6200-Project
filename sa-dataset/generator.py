c8e2c2531c617e76b756e8169ac7f44ed001d9eb
Xaler1 <alex.and.radchenko@gmail.com>
2024-04-27 00:38:23 -0400

    Dataset generation

--- sa-dataset/generator.py
+++ sa-dataset/generator.py
@@ -1,8 +1,6 @@
-import streamlit as st
-import json
 import json
 import os
-from tqdm import tqdm
+from tqdm import tqdm, trange
 import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
@@ -12,18 +10,28 @@ from streamlit_image_comparison import image_comparison
 from keys import openai_key
 import requests
 import base64
+import os
+from multiprocessing import Pool
 
 files = os.listdir("data")
-state = st.session_state
 headers = {
   "Content-Type": "application/json",
   "Authorization": f"Bearer {openai_key}"
 }
 
+prompt = """Describe what you see in the mask of the image.
+Describe the colors of the objects.
+An example output would be:
+"The table is brown, the sky is blue and the banana is yellow" 
+DO NOT say anything about the presence of a mask or the fact that the background is black. make it like a natural description. Make sure to describe ALL objects. 
+Only use the original image for context, do not include it in the output, no matter how significant.
+Keep the response under 70 words, only describe colors."""
+
 
 def rle_to_bitmap(rle):
     bitmap = coco_mask.decode(rle)
     return bitmap
+
 
 def get_cropped(id):
     with open(f"data/{files[id]}") as f:
@@ -58,6 +66,7 @@ def get_cropped(id):
     masks = cv2.resize(cropped_masks, (512, 512), interpolation=cv2.INTER_NEAREST)
     return img, masks, id
 
+
 def get_filtered(id, threshold, top=8):
     img, masks, max_id = get_cropped(id)
     areas = {}
@@ -71,22 +80,68 @@ def get_filtered(id, threshold, top=8):
     for id in to_remove:
         masks[masks == id] = 0
         areas.pop(id)
-        
-    
+
     areas = dict(sorted(areas.items(), key=lambda item: item[1], reverse=True)[:top])
-    
+
     # Remove masks that are not in the top N
     for id in np.unique(masks):
         if id not in areas:
             masks[masks == id] = 0
-    
+
     return img, masks, areas
+
 
 def encode_image(img):
     _, buffer = cv2.imencode(".jpg", img)
     return base64.b64encode(buffer).decode("utf-8")
 
-def get_chatgpt_annotation(img, masks, combination, prompt, mask_inverted, low_res):
+def get_chatgpt_body(img, masks, prompt):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    encoded_img = encode_image(img)
+    content = [
+        {
+            "type": "text",
+            "text": prompt
+        }
+    ]
+    masked = img * (masks[:, :, np.newaxis] > 0)
+    encoded_masked = encode_image(masked)
+    content.append({
+        "type": "text",
+        "text": "The original image comes first"
+    })
+    content.append({
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{encoded_img}",
+            "detail": "low"
+        }
+    })
+    content.append({
+        "type": "text",
+        "text": "The masked image comes next"
+    })
+    content.append({
+        "type": "image_url",
+        "image_url": {
+            "url": f"data:image/jpeg;base64,{encoded_masked}",
+            "detal": "low"
+        }
+    })
+    payload = {
+        "model": "gpt-4-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        "max_tokens": 70
+    }
+    return payload
+
+
+def get_chatgpt_annotation(img, masks, combination, prompt, mask_inverted):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     encoded_img = encode_image(img)
     if mask_inverted:
@@ -99,31 +154,34 @@ def get_chatgpt_annotation(img, masks, combination, prompt, mask_inverted, low_r
             "text": prompt
         }
     ]
-    masked = None
     if combination == 0:
         content.append({
             "type": "image_url",
             "image_url": {
-                "url": f"data:image/jpeg;base64,{encoded_img}"
+                "url": f"data:image/jpeg;base64,{encoded_img}",
+                "detail": "low"
             }
         })
         content.append({
             "type": "image_url",
             "image_url": {
-                "url": f"data:image/jpeg;base64,{encoded_masks}"
+                "url": f"data:image/jpeg;base64,{encoded_masks}",
+                "detail": "low"
             }
         })
     elif combination == 1:
         content.append({
             "type": "image_url",
             "image_url": {
-                "url": f"data:image/jpeg;base64,{encoded_masks}"
+                "url": f"data:image/jpeg;base64,{encoded_masks}",
+                "detail": "low"
             }
         })
         content.append({
             "type": "image_url",
             "image_url": {
-                "url": f"data:image/jpeg;base64,{encoded_img}"
+                "url": f"data:image/jpeg;base64,{encoded_img}",
+                "detail": "low"
             }
         })
     elif combination == 2:
@@ -132,10 +190,10 @@ def get_chatgpt_annotation(img, masks, combination, prompt, mask_inverted, low_r
         content.append({
             "type": "image_url",
             "image_url": {
-            "url": f"data:image/jpeg;base64,{encoded_masked}"
+                "url": f"data:image/jpeg;base64,{encoded_masked}",
+                "detail": "low"
           }
         })
-        masked = cv2.cvtColor(masked, cv2.COLOR_BGR2RGB)
     elif combination == 3:
         masked = img * (masks[:, :, np.newaxis] > 0)
         encoded_masked = encode_image(masked)
@@ -170,54 +228,67 @@ def get_chatgpt_annotation(img, masks, combination, prompt, mask_inverted, low_r
                 "role": "user",
                 "content": content
             }
-        ]
+        ],
+        "max_tokens": 70
     }
     response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return response.json()["choices"][0]["message"]["content"], masked
+    return response.json()["choices"][0]["message"]["content"]
+
+def process_image(i):
+    try:
+        destination = "test_split/"
+        img, masks, areas = get_filtered(i, 0.01, 8)
+        if len(areas) < 2:
+            return
+        plt.imsave(f"{destination}/{i}.jpg", img)
+        bw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        plt.imsave(f"{destination}/{i}_gray.jpg", bw, cmap="gray")
+        plt.imsave(f"{destination}/{i}_mask.jpg", masks, cmap="gray")
+        annotation = get_chatgpt_annotation(img, masks, 3, prompt, False)
+        with open(f"{destination}/{i}.txt", "w", encoding="utf-8") as f:
+            f.write(annotation)
+    except Exception as e:
+        return
+
+def main():
+    destination = "test_split/"
+    os.makedirs(destination, exist_ok=True)
+    n = 1000
+    with Pool(10) as p:
+        for _ in tqdm(p.imap(process_image, range(n)), total=n):
+            pass
+
+def process_batch(i):
+    destination = "train_split/"
+    img, masks, areas = get_filtered(i, 0.01, 8)
+    if len(areas) < 2:
+        return
+    plt.imsave(f"{destination}/{i}.jpg", img)
+    bw = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    plt.imsave(f"{destination}/{i}_gray.jpg", bw, cmap="gray")
+    plt.imsave(f"{destination}/{i}_mask.jpg", masks, cmap="gray")
+    payload = get_chatgpt_body(img, masks, prompt)
+    line = {
+        "custom_id": f"request-{i}",
+        "method": "POST",
+        "url": "/v1/chat/completions",
+        "body": payload
+    }
+    return line
 
 
-st.title("Mask prompt experiment")
+def batch():
+    destination = "train_split/"
+    os.makedirs(destination, exist_ok=True)
+    start = 1000
+    n = 5000
+    with Pool(16) as p:
+        for line in tqdm(p.imap(process_batch, range(start, start + n)), total=n):
+            if line:
+                with open(f"train_job.jsonl", "a", encoding="utf-8") as f:
+                    f.write(json.dumps(line) + "\n")
 
-default_prompt = """Describe what you see in the mask of the image.
-Describe the colors of the objects.
-An example output would be:
-"The table is brown, the sky is blue and the banana is yellow" 
-DO NOT say anything about the presence of a mask or the fact that the background is black. make it like a natural description. Make sure to describe ALL objects. 
-Only use the original image for context, do not include it in the output, no matter how significant.
-Stay concise, keep the response under 70 words.
-"""
 
-# Area for checkboxes and settings
-with st.container(border=True):
-    st.header("Settings")
-    id = st.number_input("Image id", min_value=0, max_value=len(files) - 1, value=0, step=1)
-    threshold = st.number_input("Mask area threshold", min_value=0.0, max_value=1.0, value=0.01, step=0.001, format="%.3f")
-    top = st.number_input("Top N masks", min_value=1, max_value=20, value=8, step=1)
-    types = ["image followed by mask", "mask followed by image", "masked image", "original followed by masked"]
-    chat_input_type = st.radio("ChatGPT image combnation type", types, index=0)
-    mask_inverted = st.checkbox("Invert the mask (will not affect the 'masked image' option)")
-    low_res = st.checkbox("Low resolution mode on OpenAI API")
-    prompt = st.text_area("ChatGPT prompt", default_prompt, height=200)
 
-chat_input_type = types.index(chat_input_type)
-
-generate = st.button("Generate")
-if generate:
-    with st.status("Processing"):
-        st.write("Resizing and filtering masks")
-        img, masks, areas = get_filtered(id, threshold, top)
-        plt.imsave("img.jpg", img)
-        plt.imsave("masks.jpg", masks)
-        st.write("Querying chatgpt")
-        response, masked = get_chatgpt_annotation(img, masks, chat_input_type, prompt, mask_inverted, low_res)
-    if chat_input_type == 2:
-        plt.imsave("masked.jpg", masked)
-        st.image("masked.jpg")
-    if chat_input_type == 3:
-        plt.imsave("masked.jpg", masked)
-        image_comparison("img.jpg", "masked.jpg", "", "")
-    else:
-        image_comparison("img.jpg", "masks.jpg", "", "")
-    st.write(response)
-
+if __name__ == "__main__":
+    batch()
